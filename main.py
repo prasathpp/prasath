fcafbdf147c60a25ce386e4a66d71f23cce5b196
prasathpp <165071232+prasathpp@users.noreply.github.com>
2024-12-17 14:53:16 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,42 @@
-from turtle import Screen
-from snake import Snake
-from food import Food
-from scorecard import ScoreCard
+# Pong Game
+
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
 import time
 
 screen = Screen()
-screen.setup(600,600)
+screen.setup(height=600, width=800)
 screen.bgcolor("black")
-screen.title("My Snake Game")
-screen.tracer()
+screen.title("Pong")
+screen.tracer(0)
 
-snake = Snake()
-food = Food()
-score_card = ScoreCard()
-
+l_paddle = Paddle((-350,0))
+r_paddle = Paddle((350,0))
+ball = Ball()
 game_is_on = True
+
+r_score = 0
+l_score = 0
+
 while game_is_on:
+    
+    time.sleep(0.07)
+    ball.move_ball()
     screen.update()
-    time.sleep(0.1)
-    snake.move()
-    # Detection of collision of food
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        score_card.increase_score()
-    # Detection of collision of wall
-    if snake.head.xcor() <= -290 or snake.head.xcor() >= 290 or snake.head.ycor() <= -290 or snake.head.ycor() >= 290:
-        game_is_on = False
-        score_card.game_over()
-    # Detection of collision of tail
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score_card.game_over()
+    # Detect collision with the wall
+    if ball.ycor() > 270 or ball.ycor() < -270:
+        ball.bounce_y()
+    # Detect collision with the paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    # Detect R paddle misses
+    if ball.xcor() > 375:
+        ball.reset_position()
+        l_score += 1
+    # Detect R paddle misses
+    if ball.xcor() < -375:
+        ball.reset_position()
+        r_score += 1
 
 screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,4 @@
+from turtle import Turtle, Screen
+
+class ScoreBoard(Turtle):
+    pass
